f8ff6d21e93f0e012953b2758d1159ec92f0eb81
amaraoussama94 <wassa36@yahoo.fr>
2023-03-20 17:26:13 +0100

    git all word  of the text

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use std::collections::HashMap;
 fn main() {
     //red the file and  build vector of individual word
     let contents = match env::args().nth(1){
+        //read the  file to string 
         Some(f) => match fs::read_to_string(f){
+            //make all text  lower case 
             Ok(s)=>s.to_lowercase(),
             Err(e)=>{
                 eprint!("could not read the file {}",e);
@@ -17,5 +19,14 @@ fn main() {
             std::process::exit(1);
         }
     };
+    //lets gett all thz word make the itor to a  vector 
+    let all_words =contents.split_whitespace().collect::<Vec<&str>>();
+    //count  how many time each uniq words occors
+    let mut word_count:HashMap<&str,u32> =HashMap::new();
+    for word in all_words.iter(){
+        *word_count.entry(word).or_insert(0)+=1;
+
+    }
+    //detrmine the most commonly used  word
 
 }
