93727bcaf77e0bba4dfeffc5bda861dcd4f803a6
amaraoussama94 <wassa36@yahoo.fr>
2023-03-20 17:11:58 +0100

    add file handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs;
+use std::collections::HashMap;
 fn main() {
-    println!("Hello, world!");
+    //red the file and  build vector of individual word
+    let contents = match env::args().nth(1){
+        Some(f) => match fs::read_to_string(f){
+            Ok(s)=>s.to_lowercase(),
+            Err(e)=>{
+                eprint!("could not read the file {}",e);
+                std::process::exit(1);
+            }
+        },
+        None=>
+        {
+            eprint!("Program requires an argument : <file path>");
+            std::process::exit(1);
+        }
+    };
+
 }
